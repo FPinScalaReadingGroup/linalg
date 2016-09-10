5cc216feb62d40f74af66935d36ab76d025926b2
Jesse Lord <jessewlord@gmail.com>
2016-09-10 07:31:22 -0700

    Prove that we have imported densevector

--- src/main/scala/linalg/hello.scala
+++ src/main/scala/linalg/hello.scala
@@ -1,5 +1,9 @@
+import breeze.linalg._
+
 object HelloWorld{
 	def main(args: Array[String]): Unit = {
 		println("hello, world! I'm going to do some linear algebra!")
+		val vector = DenseVector.zeros[Double](5)
+		println(vector)
 	}
 }
